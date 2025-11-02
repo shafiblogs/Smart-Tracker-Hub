c6a213dbe2c3cc985fb9ad3b9fbe6faf3d8dd4e0
shafi <muhammed.poyil@dewa.gov.ae>
2025-11-02 14:05:20 +0400

    New build moved to production

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.marsa.smarttrackerhub"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         val localProperties = Properties()
