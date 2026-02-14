639021d6289876cef7a95589f3a561cabf0c65d4
shafi <muhammed.poyil@dewa.gov.ae>
2026-02-14 14:17:04 +0400

    updated

--- app/src/main/java/com/marsa/smarttrackerhub/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/marsa/smarttrackerhub/ui/screens/home/HomeScreen.kt
@@ -39,8 +39,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.findRootCoordinates
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
