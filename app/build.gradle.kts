2a4f60db161171432491947b1f6a04fbc95cd89a
shafi <muhammed.poyil@dewa.gov.ae>
2026-02-24 15:48:46 +0400

    New build moved to production

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.marsa.smarttrackerhub"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "2.0"
 
         val localProperties = Properties()
         localProperties.load(FileInputStream(rootProject.file("local.properties")))
