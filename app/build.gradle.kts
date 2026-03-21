fa2b9804827d7e05ccfeb32736f05c7a71fce857
shafi <muhammed.poyil@dewa.gov.ae>
2026-03-21 10:54:28 +0400

    New build moved to production

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,13 +45,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
